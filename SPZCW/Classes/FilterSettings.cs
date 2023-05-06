6d4422490f661ac91db63090a5a79e92460078cd
IlliaShein <illiasheinillia@gmail.com>
2023-05-06 08:02:53 +0300

    FIlterSettings.cs refactorring: made code more 'clean'

--- SPZCW/Classes/FilterSettings.cs
+++ SPZCW/Classes/FilterSettings.cs
@@ -1,5 +1,6 @@
 ﻿using SPZCW.Enumerations;
 using SPZCW.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ServiceProcess;
 
@@ -17,52 +18,61 @@ namespace SPZCW.Classes
             StartModes = new List<ServiceStartMode>();
             Locations = new List<ServiceLocation>();
 
-            foreach (var choise in choices)
+            foreach (string choise in choices)
             {
-                if(choise == "Running")
-                {
-                    Statuses.Add(ServiceControllerStatus.Running);
-                }
-                else if(choise == "Stopped")
-                {
-                    Statuses.Add(ServiceControllerStatus.Stopped);
-                }
-                else if(choise == "Other")
-                {
-                    Statuses.Add(ServiceControllerStatus.Paused);
-                    Statuses.Add(ServiceControllerStatus.ContinuePending);
-                    Statuses.Add(ServiceControllerStatus.PausePending);
-                    Statuses.Add(ServiceControllerStatus.StartPending);
-                    Statuses.Add(ServiceControllerStatus.StopPending);
-                }
-                else if (choise == "Manual")
-                {
-                    StartModes.Add(ServiceStartMode.Manual);
-                }
-                else if (choise == "Automatic")
-                {
-                    StartModes.Add(ServiceStartMode.Automatic);
-                }
-                else if (choise == "Disabled")
-                {
-                    StartModes.Add(ServiceStartMode.Disabled);
-                }
-                else if (choise == "Boot")
-                {
-                    StartModes.Add(ServiceStartMode.Boot);
-                }
-                else if (choise == "System")
-                {
-                    StartModes.Add(ServiceStartMode.System);
-                }
-                else if (choise == "Localhost")
-                {
-                    Locations.Add(ServiceLocation.LocalHost);
-                }
-                else if (choise == "Another device")
-                {
-                    Locations.Add(ServiceLocation.AnotherDevice);
-                }
+                AddChoiseAsSetting(choise);
+            }
+        }
+
+        private void AddChoiseAsSetting(string choise)
+        {
+            if (choise == "Running") // Checking if choise is status 
+            {
+                Statuses.Add(ServiceControllerStatus.Running);
+            }
+            else if (choise == "Stopped")
+            {
+                Statuses.Add(ServiceControllerStatus.Stopped);
+            }
+            else if (choise == "Other")
+            {
+                Statuses.Add(ServiceControllerStatus.Paused);
+                Statuses.Add(ServiceControllerStatus.ContinuePending);
+                Statuses.Add(ServiceControllerStatus.PausePending);
+                Statuses.Add(ServiceControllerStatus.StartPending);
+                Statuses.Add(ServiceControllerStatus.StopPending);
+            }
+            else if (choise == "Manual") // Checking if choise is StartMode 
+            {
+                StartModes.Add(ServiceStartMode.Manual);
+            }
+            else if (choise == "Automatic")
+            {
+                StartModes.Add(ServiceStartMode.Automatic);
+            }
+            else if (choise == "Disabled")
+            {
+                StartModes.Add(ServiceStartMode.Disabled);
+            }
+            else if (choise == "Boot")
+            {
+                StartModes.Add(ServiceStartMode.Boot);
+            }
+            else if (choise == "System")
+            {
+                StartModes.Add(ServiceStartMode.System);
+            }
+            if (choise == "Localhost") // Checking if choise is Location 
+            {
+                Locations.Add(ServiceLocation.LocalHost);
+            }
+            else if (choise == "Another device")
+            {
+                Locations.Add(ServiceLocation.AnotherDevice);
+            }
+            else
+            {
+                throw new Exception($"Unknown setting: {choise}");
             }
         }
     }
